4a8f74399b67ae7990dd76387d19dea6a9e5bcfc
Enes <66560757+devenes@users.noreply.github.com>
2022-04-11 00:18:37 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default     = "terraform-mx"
+}
+
+variable "location" {
+  default     = "West Europe"
+  description = "The location where the resources should be created"
+}
